ad65caa986886b807ae4c28e5282281265712f20
Ali Alkadhim <aa18dg@fsu.edu>
2024-11-05 17:44:20 -0600

    particle jetnet

--- run_jetnet_diffusion.py
+++ run_jetnet_diffusion.py
@@ -13,6 +13,7 @@ from sklearn.datasets import make_moons
 # Set numpy random seed for reproducibility
 np.random.seed(42)
 import time
+import jetnet
 from jetnet.datasets import JetNet
 from jetnet.utils import jet_features
 
@@ -56,6 +57,7 @@ else:
 SUBSET = int(1e3)
 # SUBSET = -1
 x0_red = x0[:SUBSET]
+    
 
 #################### JETS ####################
 if jets_or_particles == 'jets':
@@ -118,6 +120,11 @@ if jets_or_particles == 'jets':
         
 #################### PARTICLES ####################
 elif jets_or_particles == "particles":
+    print('using substructure')
+    jet_data = jet_data[:SUBSET]
+    particle_data = particle_data[:SUBSET, :, :]
+    print(f'jet_data.shape: {jet_data.shape}')
+    print(f'particle_data.shape: {particle_data.shape}')
     num_types = len(data_args["jet_type"])
     print(f'num_types: {num_types}')
     type_indices = {jet_type: JetNet.JET_TYPES.index(jet_type) for jet_type in data_args["jet_type"]}# these are special indices for gluon, top quark, and W boson jets
@@ -163,5 +170,47 @@ elif jets_or_particles == "particles":
                     alpha_bar=alpha_bar, 
                     T=T, 
                     device=device)
+    
+     # SAMPLE
+    T_sample_1 = 1000
+    # calculate time to sample one feature
+    start_time = time.time()
+    x_sample_1 = sample_one(
+        model=epsilon_theta,
+        x0_shape=flat_x0_red.shape,
+        alpha=alpha_,
+        beta=beta,
+        alpha_bar=alpha_bar,
+        T=T_sample_1,
+        device=device,
+    )
+    x_sample_1 = x_sample_1.numpy()
+    end_time = time.time()
+    print(f'Time to sample one feature of shape={x_sample_1.shape}: {end_time - start_time:.2f} seconds')
+    
+        ### denormalize and plot
+
+    x_sample_1_denormalized = x0_mean + x_sample_1 * x0_std
+    
+    # plot samples
+    x_sample_1_denormalized = x_sample_1_denormalized.reshape(x0_red.shape)
+    
+    fig, ax = plt.subplots(1,len(selected_observables), figsize=(10,10))
+    for ind, observable in enumerate(selected_observables):
+        for jet_type in jet_types:
+            substruc_observable = get_substructure_feature(jet_data=jet_data,      
+                particle_data=x_sample_1_denormalized, 
+                type_indices=type_indices,
+                selected_observable=observable,
+                jet_type=jet_type)
+            ax[ind].hist(substruc_observable, histtype='step', label=jet_type)
+            ax[ind].set_xlabel(selected_observables_labels[ind])
+            ax[ind].legend()
+    plt.tight_layout()  
+    plt.show()
+    
+    ## evaluation metrics
+    fpnd_score = jetnet.evaluation.fpnd(x_sample_1_denormalized)
+    print(f'fpnd_score: {fpnd_score}')
     
     
